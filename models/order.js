13b39d59a43f433aacd639f1ffded12652c46938
Emmanuel Tobi <fadumilaemmanuel@gmail.com>
2025-08-07 11:06:28 +0100

    Abeg work nawwww

--- models/order.js
+++ models/order.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 
 const orderSchema = new mongoose.Schema({
-    userId:{ type: string, required: true, ref: "user"},
+    userId:{ type: String, required: true, ref: "user"},
     items:[{
         product: { type: String, required: true, ref: "product" },
         quantity: { type: Number, required: true }
